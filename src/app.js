66d823d03b226d179aab2eb4715ca9b71422ec9e
Arit Oyekan <aaoyekanmi4@gmail.com>
2020-01-07 17:44:35 -0600

    put sendFile back

--- src/app.js
+++ src/app.js
@@ -38,7 +38,13 @@ app.use(function errorHandler(error, req, res, next) {
 app.use('/api/users', require('./routes/users'))
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/books', require('./routes/books'))
-app.use(express.static(path.join(__dirname, '../client/build')));
 
+//Serve static assets in production 
+if (process.env.NODE_ENV === 'production') {
+  //set static folder
+  app.use(express.static('../client/build'));
+
+  app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, '../client','build','index.html')))
+}
 
 module.exports = app
