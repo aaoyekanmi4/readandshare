7e878d3cac266bfdcc49b04f8c477b4177011f66
Arit Oyekan <aaoyekanmi4@gmail.com>
2020-01-07 22:35:58 -0600

    npm built again after removing errors

---
+++ client/build/precache-manifest.3c714a88acce93e7de2f23da793aad0d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6eda54696f1b4b2cb676aa81b30491c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ba9dfef73307f8fc9b49",
+    "url": "/static/css/main.92e2cd37.chunk.css"
+  },
+  {
+    "revision": "9aa8ec450cd88908cf41",
+    "url": "/static/js/2.47e81ab8.chunk.js"
+  },
+  {
+    "revision": "0861ad41200203db0819c1c2e5edb5dd",
+    "url": "/static/js/2.47e81ab8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ba9dfef73307f8fc9b49",
+    "url": "/static/js/main.22f6d3d0.chunk.js"
+  },
+  {
+    "revision": "65326020070271871721",
+    "url": "/static/js/runtime-main.2df13cd9.js"
+  }
+]);
